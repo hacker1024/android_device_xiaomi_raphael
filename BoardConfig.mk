c377de7fe7a0b53bcc9cc1dd0ebf5a7714b4091f
dianlujitao <dianlujitao@lineageos.org>
2019-09-09 15:04:49 +0800

    raphael: Remove custom kernel cross compile prefix
    Change-Id: Icc58f38fa2f66b833c73902d6733f84b77dcb575
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,6 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := raphael_user_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
-  TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
   TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
 endif
 
